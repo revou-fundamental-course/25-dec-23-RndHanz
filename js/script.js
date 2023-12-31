80baeea25f8fd59a5eb8238862b6cee97c295a8e
Raihan Nur Daffa <nurdaffaraihan46@gmail.com>
2023-12-31 16:13:01 +0700

    modified content

--- js/script.js
+++ js/script.js
@@ -1,9 +1,17 @@
 function hitungluas() {
-  alas = document.getElementById("alas").value;
-  tinggi = document.getElementsByid("tinggi").value;
-  hasil = (1 / 2) * Alas * tinggi;
+  const alas = document.getElementById("alas").value;
+  const tinggi = document.getElementById("tinggi").value;
+  const hasil = 0.5 * alas * tinggi;
   document.getElementById("hasil").value = hasil;
 }
+
+// function hitungkeliling() {
+//   const sisi1 = document.getElementById("sisi1").value;
+//   const sisi2 = document.getElementById("sisi2").value;
+//   const sisi3 = document.getElementById("sisi3").value;
+//   const hasil_sisi = sisi1 + sisi2 + sisi3;
+//   document.getElementById("hasil_sisi").value = hasil_sisi;
+// }
 
 function reset() {
   document.getElementById("alas").value = "";
